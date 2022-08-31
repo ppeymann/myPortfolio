02d058390c3b137861ba9aba621f92233e043695
peyman malek <p.malek32@gmail.com>
2022-08-31 17:52:22 +0430

    make hero and about section

---
+++ src/Components/skill/Skill.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <section className='skill section' id='skills'>
+      <h2 className='section__title'>Skills</h2>
+      <span className='section__subtitle'>
+        My thechnical level
+      </span>
+      <div className='skills__container container grid'>
+          
+      </div>
+    </section>
+  )
+}
+
+export default Skill
