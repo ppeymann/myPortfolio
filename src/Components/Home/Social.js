f438ad635339ab66f843b66b67c59f34bf26c716
peyman malek <p.malek32@gmail.com>
2022-08-31 17:53:27 +0430

    made hero and about section

---
+++ src/Components/Home/Social.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social' >
+        <a href='https://www.instagram.com/p.peyman.n' className='home__social-icon' target=''>
+            <i className='uil uil-instagram'></i>
+        </a>
+        <a href='https://github.com/ppeymann' className='home__social-icon' target=''>
+          <i className='uil uil-github-alt'></i>
+        </a>
+        <a href='https://telegram.org/p_peyman_n' className='home__social-icon' target=''>
+          <i className='uil uil-telegram-alt'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
